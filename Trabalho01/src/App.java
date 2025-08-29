ab87c861d32aa0af6d1178b4a6bf7acb3d0b6c58
Matheus Kruger <matheuskruger066@gmail.com>
2025-08-28 22:00:08 -0300

    Removi o trabalho antigo

--- Trabalho01/src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("--Menu--");
-        System.out.println("1-Soma");
-        System.out.println("2-Subtração");
-        System.out.println("3-Multiplicação");
-        System.out.println("4-Divisão");
-    }
-}
