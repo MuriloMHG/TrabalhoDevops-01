07768b32c65bf3be1b7901e1d79532645653eab6
João Pedro Oliveira Braga <jpolivbraga@gmail.com>
2025-08-28 21:31:09 -0300

    adicionando esqueleto e soma

--- OneDrive/Documentos/Java/AlexandreTrabalho/src/App.java
+++ OneDrive/Documentos/Java/AlexandreTrabalho/src/App.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Escolha um número para acessar o cálculo respectivo");
+        System.out.println("1 - Adição");
+        System.out.println("2 - Subtração");
+        System.out.println("3 - Adição");
+        System.out.println("4 - Divisão");
+        int codigo = sc.nextInt();
+
+        System.out.println("Digite o primeiro número");
+        int num1 = sc.nextInt();
+        System.out.println("Digite o segundo número");
+        int num2 = sc.nextInt();
+
+        if ( codigo == 1){
+
+        int num3 =  num1 + num2;
+
+        System.out.println("Soma: " + num3);
+
+        }
+
+
     }
 }
